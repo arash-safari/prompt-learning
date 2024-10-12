0a0545cf1057a1a456b0aa206c6d360c39f26547
arash <arashsafari86@gmail.com>
2024-10-12 22:30:56 +0330

    change center class to fix error

--- train.py
+++ train.py
@@ -99,10 +99,10 @@ def extend_cfg(cfg):
    
     cfg.TRAINER.CENTERCLASS = CN()
     cfg.TRAINER.CENTERCLASS.N_CTX = 16  # number of context vectors
-    cfg.TRAINER.COOP.CSC = False  # class-specific context
-    cfg.TRAINER.COOP.CTX_INIT = ""  # initialization words
-    cfg.TRAINER.COOP.PREC = "fp16"  # fp16, fp32, amp
-    cfg.TRAINER.COOP.CLASS_TOKEN_POSITION = "end"  # 'middle' or 'end' or 'front'
+    cfg.TRAINER.CENTERCLASS.CSC = False  # class-specific context
+    cfg.TRAINER.CENTERCLASS.CTX_INIT = ""  # initialization words
+    cfg.TRAINER.CENTERCLASS.PREC = "fp16"  # fp16, fp32, amp
+    cfg.TRAINER.CENTERCLASS.CLASS_TOKEN_POSITION = "end"  # 'middle' or 'end' or 'front'
 
     cfg.TRAINER.COCOOP = CN()
     cfg.TRAINER.COCOOP.N_CTX = 16  # number of context vectors
